a8fa7824c85c119bd9d016fdd50dc5a4f7bf93c0
Harsh Poddar <poddarh@users.noreply.github.com>
2015-07-14 12:56:02 +0530

    Replaced usage of deprecated GrantedAuthorityImpl
    Replaced the deprecated GrantedAuthorityImpl class with SimpleGrantedAuthority

--- src/main/java/com/jumpbyte/test/springrest/secure/AuthenticationTokenProcessingFilter.java
+++ src/main/java/com/jumpbyte/test/springrest/secure/AuthenticationTokenProcessingFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.GenericFilterBean;
@@ -32,7 +32,7 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
                 System.out.println("valid token found");
                 
                 List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-                authorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 
                 UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("test", "test");
                 token.setDetails(new WebAuthenticationDetails((HttpServletRequest) request));
